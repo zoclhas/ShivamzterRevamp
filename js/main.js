1d5bb6a64eed9a739dbcf7d7d76c0aa7c662aa3f
Zoclhas <70476453+Zoclhas@users.noreply.github.com>
2022-12-24 21:27:00 +0400

    Few changes- Read desc
    - Uses data attr for menu toggle
    - Disables body overflow when nav menu is open
    - Changed nav menu colours
    - Changed to use SCSS variables

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-const menu = document.querySelector('.menu');
-const close = document.querySelector('.close');
-const nav = document.querySelector('nav');
+const body = document.querySelector("body");
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
 
-menu.addEventListener('click', () => {
-    nav.classList.add('open-nav');
-})
+menu.addEventListener("click", () => {
+    nav.setAttribute("data-toggled", "true");
+    body.style.overflow = "hidden";
+});
 
-close.addEventListener('click', () => {
-    nav.classList.remove('open-nav');
-})
+close.addEventListener("click", () => {
+    nav.setAttribute("data-toggled", "false");
+    body.style.overflow = "scroll";
+});
